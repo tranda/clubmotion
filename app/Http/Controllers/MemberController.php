76a653891fa75345601b876578fb22af551988dc
Zoran Trandafilovic <tranda@gmail.com>
2025-02-16 21:06:58 +0100

    imported Str

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Member;
 use Illuminate\Http\Request;
 use App\Models\MembershipCategory;
+use Illuminate\Support\Str;
 
 class MemberController extends Controller
 {
