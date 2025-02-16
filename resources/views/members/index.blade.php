82b5aafddca2e946b7a5d3ac05df1eb2b0cddfe6
Zoran Trandafilovic <tranda@gmail.com>
2025-02-16 16:18:55 +0100

    remove one spacer

--- resources/views/members/index.blade.php
+++ resources/views/members/index.blade.php
@@ -14,7 +14,6 @@
             <li><a href="#">Other</a></li>
         </ul>
     </nav>
-    <div class="spacer"></div> 
     <div class="filter-container">
         <div class="spacer"></div> 
         <form method="GET" action="{{ route('members.index') }}" class="filter-form">
