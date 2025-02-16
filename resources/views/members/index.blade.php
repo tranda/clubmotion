209b6ddb0225b098410f85f63d1e430f599ac50c
Zoran Trandafilovic <tranda@gmail.com>
2025-02-16 15:37:02 +0100

    display checkbox

--- resources/views/members/index.blade.php
+++ resources/views/members/index.blade.php
@@ -52,7 +52,7 @@
                         <td>{{ $member->email }}</td>
                         <td>{{ $member->category->category_name ?? 'N/A' }}</td>
                     <!--    <td>{{ $member->medical_validity }}</td> -->
-                        <td>{{ $member->is_active ? 'Yes' : 'No' }}</td>
+                        <td>{{ $member->is_active ? '✅' : '❌' }}</td>
                     </tr>
                 @endforeach
             </tbody>
