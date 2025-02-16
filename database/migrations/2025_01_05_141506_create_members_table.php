0ae3fd9d8684a5f082c86af94b3875f297a6756b
Zoran Trandafilovic <tranda@gmail.com>
2025-02-16 23:20:19 +0100

    removed drop table

--- database/migrations/2025_01_05_141506_create_members_table.php
+++ database/migrations/2025_01_05_141506_create_members_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::dropIfExists('members');
         Schema::create('members', function (Blueprint $table) {
             $table->id(); // Auto-incrementing primary key
             $table->string('name'); // Member's name
