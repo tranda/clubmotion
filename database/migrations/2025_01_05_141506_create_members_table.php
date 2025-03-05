f2338ffd7eea49d9096ad1c58e4a966f75fbd887
Zoran Trandafilovic <tranda@gmail.com>
2025-03-05 11:30:06 +0100

    adding nullable in migration for most params

--- database/migrations/2025_01_05_141506_create_members_table.php
+++ database/migrations/2025_01_05_141506_create_members_table.php
@@ -18,14 +18,14 @@ return new class extends Migration
             $table->id(); // Auto-incrementing primary key
             $table->string('name'); // Member's name
             $table->integer('membership_number')->unique(); // Membership number
-            $table->date('date_of_birth'); // Date of birth
-            $table->text('address'); // Member's address
-            $table->string('phone'); // Phone number
+            $table->date('date_of_birth')->nullable(); // Date of birth
+            $table->text('address')->nullable(); // Member's address
+            $table->string('phone')->nullable(); // Phone number
             $table->string('email')->unique(); // Email address
             $table->foreignId('category_id')->constrained('membership_categories')->onDelete('cascade'); // Foreign key to membership_categories table
-            $table->date('medical_validity'); // Medical validity date
+            $table->date('medical_validity')->nullable(); // Medical validity date
             $table->string('profile_image_url')->nullable(); // Profile image URL (nullable)
-            $table->string('password_hash'); // Password hash (if applicable)
+            $table->string('password_hash')->nullable(); // Password hash (if applicable)
             $table->boolean('is_active')->default(true); // Active status (default true)
             $table->string('image')->nullable(); // Image column (nullable)
             $table->timestamps(); // Timestamps for created_at and updated_at
